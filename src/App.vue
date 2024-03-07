8db2e9b936f2fe0f2dda02df92f9537353634737
Vhighc <eneh.victor@yahoo.com>
2024-03-07 15:43:30 +0100

    modal-project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,127 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <!-- templateRefs instead of queryselector -->
+  <!-- <input type="text" ref="name"> -->
+  <!-- <button @click="handleClick">click here</button> -->
+  
+  <!-- Adding props to child component -->
+  <!-- <modalScrn heading="sign up for Vhighc Enterprise" text="Grab your opportunity now"/> -->
+  
+  <!-- Data Binding -->
+  <!-- <modalScrn :heading="heading" :text="text" theme="short"/> -->
+  
+  <!-- Listening to emitted(close) Custom Events from the child(Modal) component -->
+  <!-- <div v-if="showModal"> -->
+
+    <!-- Using teleport -->
+  <teleport to="#modals" v-if="showModal">
+    <!-- <modalScrn @close="toggleModal" :heading="heading" :text="text" theme="short"/> -->
+    
+    <!-- Slots -->
+    <modalScrn @close="toggleModal" theme="short">
+      <!-- named slots -->
+      <template v-slot:links>
+        <a href=''>sign up today</a>
+        <a href=''>see more</a>
+      </template>
+      
+      <!-- default slots -->
+      <h1>sign up for Vhighc Enterprise</h1>
+      <p>Grab your opportunity now</p>
+    </modalScrn>
+  <!-- </div> -->
+
+  <!-- Using teleport -->
+</teleport>
+
+  <!-- New modal -->
+  <!-- <div v-if="showModalTwo"> -->
+
+    <!-- Using teleport -->
+    <teleport to="#modals" v-if="showModalTwo">
+    <modalScrn @close="toggleModalTwo">      
+      <h1>sign up for more update</h1>
+      <p>Get more promo codes</p>
+    </modalScrn>
+  <!-- </div> -->
+
+  <!-- Using teleport -->
+  </teleport>
+
+  <!-- Added an event modifier(.alt, .shift, .right) -->
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  
+  <!-- New modal -->
+  <button @click="toggleModalTwo">open second modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import modalScrn from "./components/Modal.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {modalScrn},
+  data() {
+    return{
+      title: "Vhighc First Vue App :)",
+      
+      // Data Binding
+      // heading: "sign up for Vhighc Enterprise",
+      // text: "Grab your opportunity now", 
+      
+      // Emitting Custom Events
+      showModal: false,
+
+      // New modal
+      showModalTwo: false
+    }
+  },
+  
+  methods: {
+    // templateRefs instead of queryselector
+    // handleClick () {
+    //   console.log(this.$refs.name)
+    //   this.$refs.name.classList.add('active')
+    //   this.$refs.name.focus()  
+    // },
+
+    // Emitting Custom Events
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+
+    // New modal
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* Teleport style modified */
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* #app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+  /* modal style clash */
+  color: blueviolet;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,108 @@
+<template>
+    <!-- Emitting custom events -->
+    <!-- <div class="backdrop" @click="closeModal"> -->
+    
+        <!-- Click event modifiers(.self) -->
+    <div class="backdrop" @click.self="closeModal">
+        <!-- Added dynamic themes -->
+        <div class="modal" :class="{ short: theme === 'short'}">
+            
+            <!-- <h1>Modal Style Clash</h1> -->
+            
+            <!-- <h1>{{heading}}</h1>
+            <p>{{text}}</p> -->
+
+            <!-- Slots -->
+            <!-- Default Slot -->
+            <slot>
+                <!-- default content only shows when no default contents is passed from the parent component -->
+            </slot>
+            
+            <!-- named slots -->
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ['heading', 'text', 'theme'],
+    
+    // Slots
+    props: ['theme'],
+    
+    // Emitting custom events
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<!-- override modal style clash #1 -->
+<style>
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+/* override modal style clash #1 */
+h1 {
+    color: aqua;
+    border: none;
+    padding: 0;
+}
+/* override global style */
+.modal p {
+    font-style: normal;
+}
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    
+}
+.modal .actions a{
+    border-radius: 4px;
+    margin: 10px;
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    text-decoration: none;
+}
+/* Styling theme(short) prop */
+.modal.short{
+    background: crimson;
+    color: white;
+}
+.modal.short h1{
+    color: white;
+}
+.modal.short .actions{
+    color: white;
+}
+.modal.short .actions a{
+    color: white;
+}
+</style>
+
+
+<!-- override modal style clash #2  -->
+<!-- <style>
+.modal h1 {
+    color: aqua;
+    border: none;
+    padding: 0;
+}
+</style> -->
